c92227c6b86b9eb2a675f37df4b45cf64a097e66
wojtek-lukowski <75492008+wojtek-lukowski@users.noreply.github.com>
2021-04-26 18:36:31 +0200

    margins & toggle

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
 
+function toggle() {
+  var x = document.getElementById('forecast');
+  if (x.className === 'hourly') {
+    x.className += ' daily';
+  } else {
+    x.className = 'hourly';
+  }
+}
+
+/*
+function toggleDaily() {
+  var x = document.getElementById('toggle-daily');
+  if (x.className === 'default') {
+    x.className += ' active';
+  } else {
+    x.className = 'default';
+  }
+}
+*/

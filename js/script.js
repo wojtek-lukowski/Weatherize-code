e929f46df8f445bb698814911d120b5bc7bea074
wojtek-lukowski <75492008+wojtek-lukowski@users.noreply.github.com>
2021-04-29 22:16:26 +0200

    hourly-daily toggle

--- js/script.js
+++ js/script.js
@@ -1,27 +1,44 @@
-/*document.querySelector('.switch').onclick = function () {
-  var x = document.getElementById('hourly')
-  var y = document.getElementById('daily')
-  if (x.style.color = 'white') {
-    x.style.color = '#a3a3a3'
-    y.style.color = 'white'
-  } else {
-    x.style.color = 'white'
-    y.style.color = '#a3a3a3'
-  }
-}*/
-
-document.querySelector('.switch').onclick = function() {
-  var x = document.getElementById('hourly')
-  var y = document.getElementById('daily')
-  if (x.className = 'active') {
-    x.className = ' default'
-    y.className = ' active'
-  } else {
-    x.className = 'active'
-    y.className = 'default'
-  }
+document.querySelector('.toggle-hourly').onclick = function() {
+  if (document.querySelector('#ForecastHourly').style.display = 'none')
+    document.querySelector('#ForecastHourly').style.display = 'flex'
+  document.querySelector('#ForecastDaily').style.display = 'none'
+  document.querySelector('#hourly-on').style.display = 'block'
+  document.querySelector('#daily-on').style.display = 'none'
 }
 
+document.querySelector('.toggle-daily').onclick = () => {
+  if (document.querySelector('#ForecastDaily').style.display = 'none')
+    document.querySelector('#ForecastHourly').style.display = 'none'
+  document.querySelector('#ForecastDaily').style.display = 'flex'
+  document.querySelector('#hourly-on').style.display = 'none'
+  document.querySelector('#daily-on').style.display = 'block'
+}
+
+document.querySelector('.toggle-hourly1').onclick = function() {
+  if (document.querySelector('#ForecastHourly').style.display = 'none')
+    document.querySelector('#ForecastHourly').style.display = 'flex'
+  document.querySelector('#ForecastDaily').style.display = 'none'
+  document.querySelector('#hourly-on').style.display = 'block'
+  document.querySelector('#daily-on').style.display = 'none'
+}
+
+document.querySelector('.toggle-daily1').onclick = () => {
+  if (document.querySelector('#ForecastDaily').style.display = 'none')
+    document.querySelector('#ForecastHourly').style.display = 'none'
+  document.querySelector('#ForecastDaily').style.display = 'flex'
+  document.querySelector('#hourly-on').style.display = 'none'
+  document.querySelector('#daily-on').style.display = 'block'
+}
+
+var x = document.querySelector('.fav')
+x.style.animationPlayState = 'paused'
+document.queryselector('.heart').onclick = () => {
+  console.log("added to favs")
+  x.style.animationPlayState = 'running'
+}
+
+
+/*
 document.querySelector('.fav').onclick = function() {
   if (document.querySelector('#heart').style.display = 'inline') {
     document.querySelector('#heart').style.display = 'none'
@@ -31,4 +48,4 @@ document.querySelector('.fav').onclick = function() {
   document.querySelector('#heart-full').style.display = 'none'
   document.querySelector('#heart').style.display = "inline"
 console.log('Removed from favs')}
-}
+}*/

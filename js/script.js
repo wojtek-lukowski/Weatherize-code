5b0d4c1cb416b95f00f7e9aa1949389a00559b74
wojtek-lukowski <75492008+wojtek-lukowski@users.noreply.github.com>
2021-05-01 17:27:57 +0200

    fav/unfav animation

--- js/script.js
+++ js/script.js
@@ -35,24 +35,27 @@ document.querySelector('.toggle-daily1').onclick = () => {
 }
 
 document.querySelector('#empty').onclick = () => {
-  // document.querySelector('.fav').style.animationPlayState = "running"
-  document.querySelector('#empty').style.display = 'none'
-  document.querySelector('#full').style.display = 'inline'
+  document.querySelector('.fav').style.animationPlayState = "running"
+
   setTimeout(function() {
+    document.querySelector('.unfav').style.animationPlayState = "paused"
+    document.querySelector('#empty').style.display = 'none'
+    document.querySelector('#full').style.display = 'inline'
     alert('Location added to favorites.')
     console.log('Added to favs')
-  }, 100);
+  }, 1100);
   console.log('animation fav')
 }
 
 document.querySelector('#full').onclick = () => {
-// document.querySelector('.unfav').style.animationPlayState = "running"
-document.querySelector('#full').style.display = 'none'
-document.querySelector('#empty').style.display = 'inline'
+document.querySelector('.unfav').style.animationPlayState = "running"
+
   setTimeout(function() {
-    // document.querySelector('.fav').style.animationPlayState = "paused"
+    document.querySelector('.fav').style.animationPlayState = "paused"
+    document.querySelector('#full').style.display = 'none'
+    document.querySelector('#empty').style.display = 'inline'
     alert('Location removed from favorites.')
     console.log("Removed from favs")
-  }, 100);
+  }, 800);
   console.log('animation unfav')
 }
